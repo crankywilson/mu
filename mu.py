feefc8f26e0c597b408fd3e79e7b8077b4723dbc
Gitea <gitea@fake.local>
2022-11-24 01:50:54 +0000

    only do sigint trick on windows

--- mu.py
+++ mu.py
@@ -2,8 +2,10 @@
 import webserver
 import wsserver
 import signal
+import os
 
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+if os.name == 'nt':  # can't get ctrl+C to work on Windows
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 webserver.start(8000)
-wsserver.start(8001, webserver.port)
+wsserver.start(8001, webserver.port) # on linux let wssserver attempt to shutdown webserver when appropriate
